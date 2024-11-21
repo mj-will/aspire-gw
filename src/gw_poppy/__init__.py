a3f5d0eda55f37ecac950186b97a52f94b2ee775
michael.williams <michael.williams@ligo.org>
2024-11-21 08:46:28 -0800

    MAINT: update for backend changes

--- src/gw_poppy/__init__.py
+++ src/gw_poppy/__init__.py
@@ -3,11 +3,8 @@ bayesian-poppy: Bayesian posterior post-processing in python
 """
 from importlib.metadata import PackageNotFoundError, version
 import logging
-from poppy.backend import set_backend
 
 from .gw_poppy import GWPoppy  
-
-set_backend()
 
 try:
     __version__ = version(__name__)
